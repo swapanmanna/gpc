b303cc8c165d4c90c05aa04596e39a5a61f67a32
Priyanshu Kundu <priyanshu.kundu@oneskai.com>
2025-02-27 12:27:22 +0530

    few other minor changes

--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -24,7 +24,7 @@ export default function AboutUs() {
                                 <span className="px-1.5 text-white text-sm font-medium leading-6">Enroll Now</span>
                             </button>
                         </div>
-                        <img className="lg:mx-0 mx-auto h-full rounded-3xl object-cover" src="https://pagedone.io/asset/uploads/1717751272.png" alt="about Us image" />
+                        <img className="lg:mx-0 mx-auto h-full rounded-3xl object-cover shadow-2xl shadow-gray-500/50 mask-image" src="https://pagedone.io/asset/uploads/1717751272.png" alt="about Us image" />
                     </div>
                 </div>
             </section>   
@@ -74,7 +74,7 @@ export default function AboutUs() {
             <section className="bg-gray-50 py-24 relative">
                 <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
                     <div className="w-full justify-start items-center gap-8 grid lg:grid-cols-2 grid-cols-1 ">
-                        <img className="lg:mx-0 mx-auto h-full rounded-3xl object-cover h-108" src={ourVision} alt="about Us image" />
+                        <img className="lg:mx-0 mx-auto h-full rounded-3xl object-cover h-108 shadow-2xl shadow-gray-500/50 mask-image" src={ourVision} alt="about Us image" />
                         <div className="w-full flex-col justify-start lg:items-start items-center gap-10 inline-flex">
                             <div className="w-full flex-col justify-start lg:items-start items-center gap-4 flex">
                                 <h2 className="text-brand-dark text-5xl font-bold font-manrope leading-normal lg:text-start text-center">Our Vision </h2>
