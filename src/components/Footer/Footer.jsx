87ee7907aedf4a064a7b206c397aa89b424bc890
Priyanshu Kundu <priyanshu.kundu@oneskai.com>
2025-02-27 11:12:21 +0530

    added link to home to the footer logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logoOne from "../../assets/logo-1.png";
 import logoTwo from "../../assets/logo-2.png";
 
@@ -9,7 +9,7 @@ const Footer = () => {
             <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
                 <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
                     <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
-                            <img className="h-20" src={logoTwo} alt="" />
+                            <Link to=""><img className="h-20" src={logoTwo} alt="" /></Link>
                         <ul className="flex items-center space-x-3 mt-6 ml-5">
                             <li>
                                 <a href="#" title="" className="flex items-center justify-center text-white transition-all duration-200 bg-gray-800 rounded-full w-7 h-7 hover:bg-[#a622e1] focus:bg-blue-600">
