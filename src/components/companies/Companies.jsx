4f542957bbc7f4790122b5adc5b29fe9462f1a3e
Priyanshu Kundu <priyanshu.kundu@oneskai.com>
2025-02-28 15:22:32 +0530

    added hp logo

--- src/components/companies/Companies.jsx
+++ src/components/companies/Companies.jsx
@@ -5,6 +5,7 @@ import gitlab from "../../assets/gitlab.png";
 import kpmg from "../../assets/kpmg.png";
 import suzlon from "../../assets/suzlon.png";
 import wellsFargo from "../../assets/wells-fargo.png";
+import hpLogo from "../../assets/hp-logo.png";
 
 export default function Companies() {
     return (
@@ -12,13 +13,14 @@ export default function Companies() {
             <div className="w-full">
                 <div className="mt-32 mb-32 ml-40 max-w-[77rem] flex flex-col items-center">
                     <h1 className="text-3xl mb-8 font-bold text-brand-dark">Our students are from 500+ corporates</h1>
-                    <div className="flex gap-[90px] justify-center mt-8">
+                    <div className="flex gap-[68px] justify-center mt-8">
                         <img src={wellsFargo} className="h-16"></img>
                         <img src={emami} className="h-16"></img>
                         <img src={gitlab} className="h-16"></img>
                         <img src={kpmg} className="h-16"></img>
                         <img src={suzlon} className="h-16"></img>
                         <img src={atlas} className="h-16"></img>
+                        <img src={hpLogo} className="h-16"></img>
                     </div>
                 </div>
             </div>
