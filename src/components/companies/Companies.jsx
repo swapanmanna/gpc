cf82e8707ba656dcca510cfe8974d048129b6335
Priyanshu Kundu <priyanshu.kundu@oneskai.com>
2025-02-28 10:54:54 +0530

    added companies section after hero

---
+++ src/components/companies/Companies.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import emami from "../../assets/emami.png";
+import atlas from "../../assets/atlas.jpg";
+import gitlab from "../../assets/gitlab.png";
+import kpmg from "../../assets/kpmg.png";
+import suzlon from "../../assets/suzlon.png";
+import wellsFargo from "../../assets/wells-fargo.png";
+
+export default function Companies() {
+    return (
+        <>
+            <div className="w-full">
+                <div className="mt-20 mb-32 ml-40 max-w-[77rem] flex flex-col items-center">
+                    <h1 className="text-3xl mb-8 font-bold text-brand-dark">Our students are from 500+ corporates</h1>
+                    <div className="flex gap-[90px] justify-center mt-8">
+                        <img src={wellsFargo} className="h-16"></img>
+                        <img src={emami} className="h-16"></img>
+                        <img src={gitlab} className="h-16"></img>
+                        <img src={kpmg} className="h-16"></img>
+                        <img src={suzlon} className="h-16"></img>
+                        <img src={atlas} className="h-16"></img>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
