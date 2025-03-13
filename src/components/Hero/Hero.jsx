4ce6366fd863d4c580c10acde4e481082dd372a1
Priyanshu Kundu <priyanshu.kundu@oneskai.com>
2025-03-13 14:12:46 +0530

    hero image of homepage reverted back to the older size

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import heroGirl from '../../assets/hero-girl-2.webp';
+import heroGirl from '../../assets/hero-girl.webp';
 import { Link } from 'react-router-dom';
 
 const Hero = () => {
