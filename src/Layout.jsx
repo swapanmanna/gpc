e1b8c1c786bcac9bea092a7f15afdc792b15bc51
Priyanshu Kundu <priyanshu.kundu@oneskai.com>
2025-03-25 10:21:12 +0530

    fixed scroll behaviour of react-router

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
+import { useLocation } from "react-router-dom";
+
+const ScrolltoTop = () => {
+
+    const {pathname} = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
+
+    return null
+}
 
 export default function Layout() {
     return (
         <>
             <Navbar />
+            <ScrolltoTop />
             <Outlet />
             <Footer />
         </>
