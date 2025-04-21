5f55eb027540cc51b1802a579c57ad4f59955783
Priyanshu Kundu <priyanshu.kundu@oneskai.com>
2025-04-21 15:22:18 +0530

    changed link,bgcolor and textcolor of the notification banner

--- src/components/Notifications/NotificationBanner.jsx
+++ src/components/Notifications/NotificationBanner.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function NotificationBanner () {
+
+    // bg -> #ffebcd
+    // text-color -> #964b00
     return (
-        <div className="fixed top-0 left-0 w-full bg-brand-purple text-gray-50 text-center h-12 flex items-center justify-center z-50">
+        <div className="fixed top-0 left-0 w-full bg-[#ffebcd] text-[#964b00] text-center h-12 flex items-center justify-center z-50">
             <p className="text-sm md:text-base">
-            👉 Notice: The CIA course syllabus has been updated! Explore the new content and <Link to="https://pages.razorpay.com/CIASessions" target="_blank" className="text-yellow-500 hover:underline">enroll now</Link> to stay ahead 👈
+            👉 Notice: The CIA course syllabus has been updated! Explore the new content and <Link to="https://rzp.io/rzp/uC1OGRim" target="_blank" className="text-yellow-900 hover:underline">enroll now</Link> to stay ahead 👈
             </p>
         </div>
     );
