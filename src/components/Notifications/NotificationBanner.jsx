4d3fa890395b058914725f1bf58f4abcd984fd5a
deathKiller10 <priyanshuwork.10@gmail.com>
2025-07-12 10:07:24 +0530

    was not fixed. tried it again

--- src/components/Notifications/NotificationBanner.jsx
+++ src/components/Notifications/NotificationBanner.jsx
@@ -21,14 +21,14 @@ export default function NotificationBanner () {
     }, [])
 
     return (
-        <div className="fixed top-0 left-0 w-screen bg-[#ffebcd] text-[#964b00] flex items-center justify-center text-center h-14 md:h-12 z-50">
+        <div className="fixed top-0 left-0 w-screen bg-[#ffebcd] text-[#964b00] flex items-center justify-center text-center h-12 z-50">
             <div
             className="w-full">
                 <motion.p
                 animate={!isMobile ? {  scale: [1, 1.1, 1] } : {}}
                 transition={!isMobile ? { duration: 2, repeat: Infinity, ease: "linear" } : {}}
                 className="md:text-sm text-xs md:p-0 px-12 font-semibold md:text-base">
-                <BsLightbulbFill className="inline text-red-600 mr-1 pb-1 h-6 w-6" />CIA Part 3 new batch starts soon! Click <span className="text-brown-700 font-semibold hover:underline"><Link to="https://youtu.be/Xn7lL67U2NI?si=OTUa4jZzE3GDJAbk" target="_blank">here</Link></span> to watch our orientation program and learn more about the course.<BsLightbulbFill className="inline text-red-600 ml-1 pb-1 h-6 w-6" />
+                <BsLightbulbFill className="inline text-red-600 mr-1 pb-1 h-4 w-4 md:h-6 md:h-6" />CIA Part 3 new batch starts soon! Click <span className="text-brown-700 font-semibold hover:underline"><Link to="https://youtu.be/Xn7lL67U2NI?si=OTUa4jZzE3GDJAbk" target="_blank">here</Link></span> to watch our orientation program and learn more about the course.<BsLightbulbFill className="inline text-red-600 ml-1 pb-1 h-4 w-4 md:h-6 md:h-6" />
                 </motion.p>
             </div>
         </div>
