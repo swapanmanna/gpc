85fc3a019985f7a3edeaeffba73333a3fbef7037
deathKiller10 <priyanshuwork.10@gmail.com>
2025-07-12 10:01:15 +0530

    fixed notif banner overflow issue

--- src/components/Notifications/NotificationBanner.jsx
+++ src/components/Notifications/NotificationBanner.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import { BsLightbulbFill } from "react-icons/bs"
 import { motion } from "motion/react";
@@ -8,13 +8,25 @@ export default function NotificationBanner () {
     // bg -> #ffebcd
     // text-color -> #964b00
     // text-brown-700 font-semibold hover:underline
+
+    const [isMobile, setIsMobile] = useState(false)
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768)
+        }
+
+        handleResize()
+        window.addEventListener("resize", handleResize)
+        return () => window.removeEventListener("resize", handleResize)
+    }, [])
+
     return (
         <div className="fixed top-0 left-0 w-screen bg-[#ffebcd] text-[#964b00] flex items-center justify-center text-center h-12 z-50">
             <div
             className="w-full">
                 <motion.p
-                animate={{  scale: [1, 1.1, 1] }}
-                transition={{ duration: 2, repeat: Infinity, ease: "linear" }}
+                animate={!isMobile ? {  scale: [1, 1.1, 1] } : {}}
+                transition={!isMobile ? { duration: 2, repeat: Infinity, ease: "linear" } : {}}
                 className="md:text-sm text-xs md:p-0 px-12 font-semibold md:text-base">
                 <BsLightbulbFill className="inline text-red-600 mr-1 pb-1 h-6 w-6" />CIA Part 3 new batch starts soon! Click <span className="text-brown-700 font-semibold hover:underline"><Link to="https://youtu.be/Xn7lL67U2NI?si=OTUa4jZzE3GDJAbk" target="_blank">here</Link></span> to watch our orientation program and learn more about the course.<BsLightbulbFill className="inline text-red-600 ml-1 pb-1 h-6 w-6" />
                 </motion.p>
