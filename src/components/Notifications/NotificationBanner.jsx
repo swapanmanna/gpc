21bce0c41f554316a77724d522cd38fe1370046c
deathKiller10 <priyanshuwork.10@gmail.com>
2025-07-17 01:07:13 +0530

    updated notification banner

--- src/components/Notifications/NotificationBanner.jsx
+++ src/components/Notifications/NotificationBanner.jsx
@@ -9,18 +9,16 @@ export default function NotificationBanner () {
     // text-color -> #964b00
     // text-brown-700 font-semibold hover:underline
 
-    let windowSize = window.innerWidth
-
     const [isMobile, setIsMobile] = useState(false)
     useEffect(() => {
         const handleResize = () => {
-            setIsMobile(windowSize < 768)
+            setIsMobile(window.innerWidth < 768)
         }
 
         handleResize()
         window.addEventListener("resize", handleResize)
         return () => window.removeEventListener("resize", handleResize)
-    }, [])
+    }, [window.innerWidth])
 
     return (
         <div className="fixed top-0 left-0 w-screen bg-[#ffebcd] text-[#964b00] flex items-center justify-center text-center h-12 z-50">
@@ -32,11 +30,11 @@ export default function NotificationBanner () {
                     animate={{  scale: [1, 1.1, 1] }}
                     transition={{ duration: 2, repeat: Infinity, ease: "linear" }}
                     className="md:text-sm text-xs md:p-0 px-8 font-semibold md:text-base">
-                    <BsLightbulbFill className="inline text-red-600 mr-1 pb-1 h-4 w-4 md:h-6 md:h-6" />CIA Part 3 new batch starts soon! Click <span className="text-brown-700 font-semibold hover:underline"><Link to="https://youtu.be/Xn7lL67U2NI?si=OTUa4jZzE3GDJAbk" target="_blank">here</Link></span> to watch our orientation program and learn more about the course.<BsLightbulbFill className="inline text-red-600 ml-1 pb-1 h-4 w-4 md:h-6 md:h-6" />
+                    <BsLightbulbFill className="inline text-red-600 mr-1 pb-1 h-4 w-4 md:h-6 md:h-6" />Not sure how to start your CIA journey? Explore our <Link className="text-red-600 hover:underline" to="https://www.youtube.com/@global-professional-cert" target="_blank">youtube channel</Link> or <Link className="text-red-600" to="/contact">contact us</Link> to get started!<BsLightbulbFill className="inline text-red-600 ml-1 pb-1 h-4 w-4 md:h-6 md:h-6" />
                     </motion.p> 
                     : 
                     <p className="md:text-sm text-xs md:p-0 px-8 font-semibold md:text-base">
-                    CIA Part 3 new batch starts soon! Click <span className="text-brown-700 font-semibold hover:underline"><Link to="https://youtu.be/Xn7lL67U2NI?si=OTUa4jZzE3GDJAbk" target="_blank">here</Link></span> to watch our orientation program and learn more about the course.
+                    Not sure how to start your CIA journey? Explore our <Link to="https://www.youtube.com/@global-professional-cert" target="_blank" className="text-red-600 hover:underline">youtube channel</Link> or <Link className="text-red-600" to="/contact">contact us</Link> to get started!
                     </p>
                 }
             </div>
